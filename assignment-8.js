aef290473db1540458f885cc6c66bea7f936363a
kasarla sai vivek <vivekkasarla781@gmail.com>
2026-01-25 09:50:51 +0530

    Add files via upload

---
+++ assignment-8.js
@@ -0,0 +1,34 @@
+// Hands-on 1
+const user = {
+    id: 101,
+    name: "Ravi",
+    preferences: {
+        theme: "dark",
+        language: "en"
+    }
+};
+
+let userCopy = structuredClone(user);
+userCopy.preferences.theme = "light";
+console.log(user, userCopy);
+
+
+// Hands-on 2
+const order = {
+    orderId: "ORD1001",
+    customer: {
+        name: "Anita",
+        address: {
+            city: "Hyderabad",
+            pincode: 500085
+        }
+    },
+    items: [
+        { product: "Laptop", price: 70000 }
+    ]
+};
+
+let orderCopy = structuredClone(order);
+orderCopy.customer.address.city="karimnagar";
+orderCopy.items[0].price=80000;
+console.log(order,orderCopy);
